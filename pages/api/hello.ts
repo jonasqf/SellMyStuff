296c0c003b7cd1fd372161204c327401e7121c8b
Jonas Flach <joansqf@gmail.com>
2022-07-22 22:13:34 -0300

    changed type Data to product atributes

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  title: string,
+  description: string,
+  price: number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ title: 'John Doe', description: "Mixer Philips Walita", price: 120.00 })
 }
